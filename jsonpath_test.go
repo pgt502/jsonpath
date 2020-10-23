c1799140631158ab5aa20c96710681c5e9e6a4f1
pawel <pawel@getbequest.com>
2020-10-23 14:19:13 +0100

    changed the signature of the Marshal function to take map[string]interface{} as input. Added a more complex object array test

--- jsonpath_test.go
+++ jsonpath_test.go
@@ -2,9 +2,10 @@ package jsonpath_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
-	. "github.com/cthulhu/jsonpath"
+	. "github.com/pgt502/jsonpath"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -13,7 +14,7 @@ import (
 var _ = Describe("Jpath", func() {
 	Context("Simple key value", func() {
 		It("generates json", func() {
-			in := map[string]string{"key": "value"}
+			in := map[string]interface{}{"key": "value"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"key":"value"}`)))
@@ -21,7 +22,7 @@ var _ = Describe("Jpath", func() {
 	})
 	Context("Simple embeddens key value", func() {
 		It("generates json", func() {
-			in := map[string]string{"price.value": "100.00"}
+			in := map[string]interface{}{"price.value": "100.00"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"price":{"value":"100.00"}}`)))
@@ -29,19 +30,19 @@ var _ = Describe("Jpath", func() {
 	})
 	Context("Panic atack with number and dot", func() {
 		It("generates json skipping the wrong keys - first hashes", func() {
-			in := map[string]string{"one": "1233", "2. subcategory": "booooom", "two": "2"}
+			in := map[string]interface{}{"one": "1233", "2. subcategory": "booooom", "two": "2"}
 			_, err := Marshal(in)
 			Expect(err).To(HaveOccurred())
 		})
 		It("generates json skipping the wrong keys - first arrays", func() {
-			in := map[string]string{"2. subcategory": "booooom", "one": "1233", "two": "2"}
+			in := map[string]interface{}{"2. subcategory": "booooom", "one": "1233", "two": "2"}
 			_, err := Marshal(in)
 			Expect(err).To(HaveOccurred())
 		})
 	})
 	Context("Long embeddens key value", func() {
 		It("generates json", func() {
-			in := map[string]string{"price.value1.value2": "100.00"}
+			in := map[string]interface{}{"price.value1.value2": "100.00"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"price":{"value1":{"value2":"100.00"}}}`)))
@@ -49,7 +50,7 @@ var _ = Describe("Jpath", func() {
 	})
 	Context("Simple embeddens few key values", func() {
 		It("generates json", func() {
-			in := map[string]string{"price.value": "100.00", "price.currency": "EU"}
+			in := map[string]interface{}{"price.value": "100.00", "price.currency": "EU"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"price":{"currency":"EU","value":"100.00"}}`)))
@@ -57,7 +58,7 @@ var _ = Describe("Jpath", func() {
 	})
 	Context("Simple embeddens few key values different levels", func() {
 		It("generates json", func() {
-			in := map[string]string{"price.value": "100.00", "price.currency": "EU", "shipping.value": "99.00", "shipping.currency": "UA"}
+			in := map[string]interface{}{"price.value": "100.00", "price.currency": "EU", "shipping.value": "99.00", "shipping.currency": "UA"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"price":{"currency":"EU","value":"100.00"},"shipping":{"currency":"UA","value":"99.00"}}`)))
@@ -65,7 +66,7 @@ var _ = Describe("Jpath", func() {
 	})
 	Context("Simple embeddens few key values and array with one value", func() {
 		It("generates json", func() {
-			in := map[string]string{"prices.0": "100.00"}
+			in := map[string]interface{}{"prices.0": "100.00"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"prices":["100.00"]}`)))
@@ -73,7 +74,7 @@ var _ = Describe("Jpath", func() {
 	})
 	Context("Simple embeddens few key values and array with two values", func() {
 		It("generates json", func() {
-			in := map[string]string{"prices.1": "100.00", "prices.0": "10.00"}
+			in := map[string]interface{}{"prices.1": "100.00", "prices.0": "10.00"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"prices":["10.00","100.00"]}`)))
@@ -81,7 +82,7 @@ var _ = Describe("Jpath", func() {
 	})
 	Context("Simple embeddens few key values and array with three values", func() {
 		It("generates json", func() {
-			in := map[string]string{"prices.2": "100.00", "prices.0": "10.00"}
+			in := map[string]interface{}{"prices.2": "100.00", "prices.0": "10.00"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"prices":["10.00",null,"100.00"]}`)))
@@ -89,7 +90,7 @@ var _ = Describe("Jpath", func() {
 	})
 	Context("Simple embeddens few key values and array with shipping", func() {
 		It("generates json", func() {
-			in := map[string]string{"price.value": "100.00", "price.currency": "EU", "shipping.0.country": "GB", "shipping.0.service": "Standart shipping", "shipping.0.price.value": "33", "shipping.0.price.curency": "GBP"}
+			in := map[string]interface{}{"price.value": "100.00", "price.currency": "EU", "shipping.0.country": "GB", "shipping.0.service": "Standart shipping", "shipping.0.price.value": "33", "shipping.0.price.curency": "GBP"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"price":{"currency":"EU","value":"100.00"},"shipping":[{"country":"GB","price":{"curency":"GBP","value":"33"},"service":"Standart shipping"}]}`)))
@@ -98,15 +99,52 @@ var _ = Describe("Jpath", func() {
 	Context("Arrays", func() {
 		Context("key value", func() {
 			It("generates json", func() {
-				in := map[string]string{"0.value": "100.00"}
+				in := map[string]interface{}{"0.value": "100.00"}
 				actual, err := Marshal(in)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(actual).To(Equal([]byte(`[{"value":"100.00"}]`)))
 			})
+			It("should work with a more complex object", func() {
+				in := map[string]interface{}{
+					"people.0.name": "John",
+					"people.0.age":  20,
+					"people.0.address": map[string]interface{}{
+						"line1": "10 Downing Street", "city": "London",
+					},
+					"people.1.name": "Bob",
+					"people.1.age":  24,
+					"people.1.address": map[string]interface{}{
+						"line1": "33 Oxford Street", "city": "London",
+					},
+				}
+				expected := `{"people":[
+					{
+						"name" : "John",
+						"age" : 20,
+						"address" : {
+							"line1" : "10 Downing Street",
+							"city" : "London"
+						}
+					},
+					{
+						"name" : "Bob",
+						"age" : 24,
+						"address" : {
+							"line1" : "33 Oxford Street",
+							"city" : "London"
+						}
+					}
+				]}`
+				actual, err := Marshal(in)
+				Expect(err).NotTo(HaveOccurred())
+				fmt.Printf("actual: %s\n", actual)
+				Expect(actual).To(MatchJSON(expected))
+			})
+
 		})
 		Context("array key", func() {
 			It("generates array", func() {
-				in := map[string]string{"0.value.[]": "1,2,3,4"}
+				in := map[string]interface{}{"0.value.[]": "1,2,3,4"}
 				actual, err := Marshal(in)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(actual).To(Equal([]byte(`[{"value":["1","2","3","4"]}]`)))
@@ -114,7 +152,7 @@ var _ = Describe("Jpath", func() {
 		})
 		Context("key value with num", func() {
 			It("generates json", func() {
-				in := map[string]string{"0.value.num()": "100.00"}
+				in := map[string]interface{}{"0.value.num()": "100.00"}
 				actual, err := Marshal(in)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(actual).To(Equal([]byte(`[{"value":100}]`)))
@@ -122,7 +160,7 @@ var _ = Describe("Jpath", func() {
 		})
 		Context("key value with float", func() {
 			It("generates json", func() {
-				in := map[string]string{"0.value.num()": "100.12"}
+				in := map[string]interface{}{"0.value.num()": "100.12"}
 				actual, err := Marshal(in)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(actual).To(Equal([]byte(`[{"value":100.12}]`)))
@@ -130,25 +168,25 @@ var _ = Describe("Jpath", func() {
 		})
 		Context("key value with bool", func() {
 			It("generates json for true", func() {
-				in := map[string]string{"0.value.bool()": "true"}
+				in := map[string]interface{}{"0.value.bool()": "true"}
 				actual, err := Marshal(in)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(actual).To(Equal([]byte(`[{"value":true}]`)))
 			})
 			It("generates json for false", func() {
-				in := map[string]string{"0.value.bool()": "false"}
+				in := map[string]interface{}{"0.value.bool()": "false"}
 				actual, err := Marshal(in)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(actual).To(Equal([]byte(`[{"value":false}]`)))
 			})
 			It("generates json for empty", func() {
-				in := map[string]string{"0.value.bool()": ""}
+				in := map[string]interface{}{"0.value.bool()": ""}
 				actual, err := Marshal(in)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(actual).To(Equal([]byte(`[{"value":false}]`)))
 			})
 			It("generates json for not bool value", func() {
-				in := map[string]string{"0.value.bool()": "1234"}
+				in := map[string]interface{}{"0.value.bool()": "1234"}
 				actual, err := Marshal(in)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(actual).To(Equal([]byte(`[{"value":false}]`)))
@@ -157,7 +195,7 @@ var _ = Describe("Jpath", func() {
 	})
 	Measure("it should do something hard efficiently", func(b Benchmarker) {
 		runtime := b.Time("runtime", func() {
-			in := map[string]string{"price.value": "100.00", "price.currency": "EU", "shipping.0.country": "GB", "shipping.0.service": "Standart shipping", "shipping.0.price.value": "33", "shipping.0.price.curency": "GBP"}
+			in := map[string]interface{}{"price.value": "100.00", "price.currency": "EU", "shipping.0.country": "GB", "shipping.0.service": "Standart shipping", "shipping.0.price.value": "33", "shipping.0.price.curency": "GBP"}
 			actual, err := Marshal(in)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal([]byte(`{"price":{"currency":"EU","value":"100.00"},"shipping":[{"country":"GB","price":{"curency":"GBP","value":"33"},"service":"Standart shipping"}]}`)))
@@ -167,46 +205,46 @@ var _ = Describe("Jpath", func() {
 })
 
 func BenchmarkComplexJSONPathArray(b *testing.B) {
-	in := map[string]string{"price.value": "100.00", "price.currency": "EU", "shipping.0.country": "GB", "shipping.0.service": "Standart shipping", "shipping.0.price.value": "33", "shipping.0.price.curency": "GBP"}
+	in := map[string]interface{}{"price.value": "100.00", "price.currency": "EU", "shipping.0.country": "GB", "shipping.0.service": "Standart shipping", "shipping.0.price.value": "33", "shipping.0.price.curency": "GBP"}
 	for n := 0; n < b.N; n++ {
 		Marshal(in)
 	}
 }
 func BenchmarkSimpleJSONPathArrayWithNum(b *testing.B) {
-	in := map[string]string{"0.value.num()": "100.12"}
+	in := map[string]interface{}{"0.value.num()": "100.12"}
 	for n := 0; n < b.N; n++ {
 		Marshal(in)
 	}
 }
 func BenchmarkSimpleJSONPathArrayWithBool(b *testing.B) {
-	in := map[string]string{"0.value.bool()": "true"}
+	in := map[string]interface{}{"0.value.bool()": "true"}
 	for n := 0; n < b.N; n++ {
 		Marshal(in)
 	}
 }
 
 func BenchmarkSimpleJSONPathArrayInsideArray(b *testing.B) {
-	in := map[string]string{"0.value.[]": "1,2,3,4,5,6"}
+	in := map[string]interface{}{"0.value.[]": "1,2,3,4,5,6"}
 	for n := 0; n < b.N; n++ {
 		Marshal(in)
 	}
 }
 
 func BenchmarkSimpleJSONPathArrays(b *testing.B) {
-	in := map[string]string{"value.[]": "1,2,3,4,5,6"}
+	in := map[string]interface{}{"value.[]": "1,2,3,4,5,6"}
 	for n := 0; n < b.N; n++ {
 		Marshal(in)
 	}
 }
 
 func BenchmarkSimpleJSONPathSimple(b *testing.B) {
-	in := map[string]string{"value": "100.12"}
+	in := map[string]interface{}{"value": "100.12"}
 	for n := 0; n < b.N; n++ {
 		Marshal(in)
 	}
 }
 func BenchmarkJSONNative(b *testing.B) {
-	in := map[string]string{"0.value.num()": "100.12"}
+	in := map[string]interface{}{"0.value.num()": "100.12"}
 	for n := 0; n < b.N; n++ {
 		json.Marshal(in)
 	}
